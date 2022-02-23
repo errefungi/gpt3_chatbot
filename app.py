010d38567b66ea7f283e8ec7cea3d4f7dc907e97
ivan rodriguez <santiago_rod.ramirez@uao.edu.co>
2022-02-23 06:14:51 -0500

    Modifique app.py desde la rama app

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
+from json.tool import main
+
+
 este es el archivo de la app con flask
 Wait, estoy probando el protocolo SSH
+def start():
+    if __name__ == '__main__':
+        print('Este es el nuevo archivo del chatbot')
